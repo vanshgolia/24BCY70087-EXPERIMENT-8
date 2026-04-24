6467e04c1a6af7cf2f6c9a5d092cf1711bbb22a5
vanshgolia <vanshror77@gmail.com>
2026-04-24 11:37:58 +0530

    Create App.jsx

---
+++ experiment-3.1.2/jwt-auth-app/client/src/App.jsx
@@ -0,0 +1,26 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
